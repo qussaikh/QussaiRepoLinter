847d43064e08a7c7858de7c8b7752e4aa7cace36
Qussai Khalil <106125439+qussaikh@users.noreply.github.com>
2024-01-29 10:30:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let x = 5
-	let y = 7
+const x = 5
+const y = 7
 
-function double(x) { return x*2; }
+function double (x) { return x * 2 }
 
-console.print(" Dubbelt av 5 är " + double(x));
+console.print(' Dubbelt av 5 är ' + double(x) + double(y))
